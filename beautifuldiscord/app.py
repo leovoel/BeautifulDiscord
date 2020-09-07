44ea225bee249ab63e93d68ee1ef8d09d1c29860
Rapptz <rapptz@gmail.com>
2020-09-07 18:15:55 -0400

    Try to play nicely with CSP again.

--- beautifuldiscord/app.py
+++ beautifuldiscord/app.py
@@ -202,10 +202,11 @@ def revert_changes(discord):
 def allow_https():
     bypass_csp = textwrap.dedent("""
     require("electron").session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders }, done) => {
-      Object.keys(responseHeaders)
-      .filter(k => (/^content-security-policy/i).test(k) || (/^x-frame-options/i).test(k))
-      .map(k => (delete responseHeaders[k]));
-
+      let csp = responseHeaders["content-security-policy"];
+      if (!csp) return done({cancel: false});
+      let header = csp[0].replace(/connect-src ([^;]+);/, "connect-src $1 https://*;");
+      header = header.replace(/style-src ([^;]+);/, "style-src $1 https://*;");
+      responseHeaders["content-security-policy"] = header;
       done({ responseHeaders });
     });
     """)
